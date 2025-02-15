bba86c9781cb3a59d3c585b9c4891ca9cf6cfed4
tulsirampathe <tulsirampathe81@gmail.com>
2025-02-15 23:32:38 +0530

    resolved merge conflicts

--- frontend/src/components/Footer.jsx
+++ frontend/src/components/Footer.jsx
@@ -1,7 +1,9 @@
 import React from "react";
 import { assets } from "../assets/assets";
+import { useNavigate } from "react-router-dom";
 
 const Footer = () => {
+  const navigate = useNavigate();
   return (
     <div className="md:mx-10">
       <div className="flex flex-col sm:grid grid-cols-[3fr_1fr_1fr] gap-14 my-10  mt-40 text-sm">
@@ -16,24 +18,18 @@ const Footer = () => {
 
         <div>
           <p className="text-xl font-medium mb-5">COMPANY</p>
-          <ul className="flex flex-col gap-2 text-gray-600">
-            <li>Home</li>
-            <li>About us</li>
+          <ul className="flex flex-col gap-2 text-gray-600 cursor-pointer">
+            <li onClick={() => {navigate("/"); scrollTo(0, 0)}}>Home</li>
+            <li onClick={() => {navigate("/about"); scrollTo(0, 0)}}>About us</li>
             <li>Delivery</li>
             <li>Privacy policy</li>
           </ul>
         </div>
 
         <div>
-<<<<<<< HEAD
           <p className="text-xl font-medium mb-5">GET IN TOUCH</p>
           <ul className="flex flex-col gap-2 text-gray-600">
             <li>+91 7692057868</li>
-=======
-          <p className='text-xl font-medium mb-5'>GET IN TOUCH</p>
-          <ul className='flex flex-col gap-2 text-gray-600'>
-            <li>+1-212-459-7890</li>
->>>>>>> ff48a73d192ee82b83cc68b95594afb826565736
             <li>sympcare@gmail.com</li>
           </ul>
         </div>
@@ -41,13 +37,9 @@ const Footer = () => {
 
       <div>
         <hr />
-<<<<<<< HEAD
         <p className="py-5 text-sm text-center">
           Copyright 2025 @ SympCare - All Right Reserved.
         </p>
-=======
-        <p className='py-5 text-sm text-center'>Copyright 2025 @ Sympcare.com - All Right Reserved.</p>
->>>>>>> ff48a73d192ee82b83cc68b95594afb826565736
       </div>
     </div>
   );
