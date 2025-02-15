0c249f3ba8311eb040a50d998fdca298e8ab6ed1
Shivnandan Verma <shivnandanverma918@gmail.com>
2025-02-15 22:45:49 +0530

    gg

--- frontend/src/pages/Contact.jsx
+++ frontend/src/pages/Contact.jsx
@@ -15,7 +15,7 @@ const Contact = () => {
           <p className=' font-semibold text-lg text-gray-600'>OUR OFFICE</p>
           <p className=' text-gray-500'>54709 Willms Station <br /> Suite 350, Washington, USA</p>
           <p className=' text-gray-500'>Tel: (415) 555-0132 <br /> Email: greatstackdev@gmail.com</p>
-          <p className=' font-semibold text-lg text-gray-600'>CAREERS AT PRESCRIPTO</p>
+          <p className=' font-semibold text-lg text-gray-600'>CAREERS AT Sympcare</p>
           <p className=' text-gray-500'>Learn more about our teams and job openings.</p>
           <button className='border border-black px-8 py-4 text-sm hover:bg-black hover:text-white transition-all duration-500'>Explore Jobs</button>
         </div>
