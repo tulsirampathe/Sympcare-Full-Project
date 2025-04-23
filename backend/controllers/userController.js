4e7d485da8b003bc9eb63bfafc17cdea9100239d
shivnandanverma918@gmail.com <shivnandanverma918@gmail.com>
2025-04-23 11:57:21 +0530

    whatsapp message

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -178,7 +178,7 @@ const bookAppointment = async (req, res) => {
         // save new slots data in docData
         await doctorModel.findByIdAndUpdate(docId, { slots_booked })
 
-        res.json({ success: true, message: 'Appointment Booked' })
+        res.json({ success: true, message: 'Appointment Booked' , appointmentData})
 
     } catch (error) {
         console.log(error)
